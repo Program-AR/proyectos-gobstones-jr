84f9a8e4f4243ba98dde51c76f161fcab1371472
Franco Frizzo <francofrizzo@gmail.com>
2018-10-17 12:16:23 -0300

    Agrego capturas SVG de caps. 2 y 3

---
+++ WIP/renombrar-archivos-svg.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const yaml = require("js-yaml");
+
+// Load info-proyectos.yaml
+const infoAllRaw = fs.readFileSync("../info-proyectos.yaml");
+const infoAll = yaml.load(infoAllRaw);
+
+let filenames = fs.readdirSync(".").filter(filename => filename.endsWith(".svg"));
+
+filenames.forEach(filename => {
+    let filenameParts = filename.match(/^(\d\d\d)(-.*)/);
+    if (filenameParts) {
+        let projectNumber = filenameParts[1];
+        let projectName = infoAll
+            .find(cap => cap.id == "Cap."+projectNumber[0])
+            .exercises
+            .find(exercise => exercise.new.id == projectNumber)
+            .new.name
+            .replace(/[.…:<>"*\\/|¡!¿?]/g, "");
+        console.log("Renaming " + projectNumber + "." + projectName + filenameParts[2]);
+        fs.renameSync(filename, projectNumber + "." + projectName + filenameParts[2]);
+    }
+})
+
+// console.log(files);
